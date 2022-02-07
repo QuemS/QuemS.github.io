932a5afec22e2e52f2bf313e3ee659195296f36d
Sushchenok Mykhailo <hostkillcar@gmail.com>
2022-02-07 20:41:28 +0200

    v0.01

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,70 +1,78 @@
-import { masterCardArr, maestroArr } from './module.js'
 
-class Card{
-    constructor(numberCard){
-        this.card = numberCard;
-    }
-    convert(){
-        let newArr = this.card.split('').reverse().map(i =>+i)
-        return newArr;
-    }
-    numberTwo(){
-        return this.convert().reverse().splice(0,2).join("");
-    }
-    numberFour(){
-        return this.convert().reverse().splice(0,4).join("");
-    }
-    numberSix(){
-        return this.convert().reverse().splice(0,6).join("");
-    }
-    isCorrect(){
-        let sum = 0 ;
-        this.convert().map( (item,i) =>{
-            if (i % 2 == 0) {
-                sum = sum + this.convert()[i];
-            }else{
-                let double = this.convert()[i] * 2;
-                sum = sum + (double > 9 ? double - 9: double);
-            }
-            
-        });
-        return sum % 10 == 0;
-    }
-    visa(){
-        return (this.convert().reverse()[0] == 4 && this.convert().length >=13 && this.convert().length <= 19? true : false);
-    }
-    maestro(){
-        if (this.convert().length >= 12 && this.convert().length <= 19 ) {
-            
-            return (maestroArr.find( item =>  this.numberFour() == item)) ? true: false;
-        }
-    }
-    masterCard() {
-        if (this.convert().length == 16) {
-            if (this.numberTwo() >= masterCardArr[0] && this.numberTwo() <= masterCardArr[1]) {
-                return true;
-            } else if (this.numberSix() >= masterCardArr[2] && this.numberSix() <= masterCardArr[3]) {
-                return true
-            }else{
-                return false
-            }
-        }
+
+let requestNbu = await fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json');
+    requestNbu = await requestNbu.json();
+
+let requestCountry = await fetch('https://restcountries.com/v3.1/all')
+    requestCountry = await requestCountry.json();
+
+let getNewNbu = requestNbu.map(item => ({
+        currency: item.txt,
+        cc: item.cc,
+        rate: item.rate,
+        flags: [],
+        name: []
+
+    }));
+
+let newArrCountry = requestCountry.map(item =>  ({
+        name: item.translations.rus.common,
+        currencies: item.currencies,
+        flags: item.flags.png
+    }) );
+
+
+for (let i = 0; i < getNewNbu.length; i++) {
+    for (let j = 0; j < newArrCountry.length; j++) {
+        if (newArrCountry[j].currencies !== undefined && `${getNewNbu[i].cc}` in newArrCountry[j].currencies){
+            getNewNbu[i].flags.push(`${newArrCountry[j].flags}`);
+            getNewNbu[i].name.push(`${newArrCountry[j].name}`);
+        } 
     }
 }
 
+let resultArr = [];
+    resultArr = getNewNbu;
 
-let inputCard = prompt('Введите номер карты');
-let a = new Card(inputCard);
-
-
-if (a.visa() && a.isCorrect()) {
-    alert (`Карта: ${inputCard} VISA`)
-}else if (a.maestro() && a.isCorrect()) {
-    alert (`Карта: ${inputCard} MAESTRO`)
-}else if (a.masterCard() && a.isCorrect()) {
-    alert (`Карта: ${inputCard} MASTERCARD`)
-}else if (a.isCorrect()) {
-    alert (`Карта: ${inputCard} Другая`)
-}else{
-    alert (`Неверный ввод`)
+for (let j = 0; j < resultArr.length - 1; j++) {
+    for (let i = 0; i < resultArr.length - 1 - j; i++) {
+        
+        if (resultArr[i].flags.length < resultArr[i + 1].flags.length ) {
+            let temp = resultArr[i];
+            resultArr[i] = resultArr[i + 1];
+            resultArr[i + 1] = temp;
+        }
+    }
 }
+app.innerHTML = resultArr.map( (item,index)=>{
+        let flags = '';
+        for (let i = 0; i < item.flags.length; i++) {
+            
+            flags+= `<img src="${item.flags[i]}" alt="" class="mx-1" title="${item.name[i]}">`;
+        }
+        return `
+        <tr>
+        <th scope="row">${index + 1}</th>
+            <td>${item.currency} ${item.cc} </td>
+            <td>${item.rate.toFixed(2)}</td>
+            <td class="text-start">
+            ${flags}
+            </td>
+        </tr>`
+        
+    }).join('');
+
+
+    console.log(requestCountry);
+
+
+//<a href="https://www.google.com/maps/place/Bosnia+and+Herzegovina" target="_blank"><img src="${item.flags[i]}" alt="" class="mx-1" title="${item.name[i]}"></a>
+  
+
+
+
+         
+
+
+
+
--- assets/js/module.js
+++
@@ -1,3 +0,0 @@
-let masterCardArr = [51, 55, 222100, 272099];
-let maestroArr = ['5018', '5020', '5038', '5893', '6304', '6759', '6761', '6762', '6763', '0604', '6390'];
-export { masterCardArr, maestroArr }
